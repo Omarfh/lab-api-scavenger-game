ea99d31bd64575a52f632ef3a260bac8b3fa2eb3
Omarfh <clarineteomar@ciencias.unam.mx>
2022-01-03 21:25:51 -0600

    laso

--- challenge-1.py
+++ challenge-1.py
@@ -1,1 +1,15 @@
 # enter your code below
+from github import Github
+
+token = '<Your token here>'
+github = Github(token)
+user = github.get_user()
+# user.login
+repo = github.get_repo('ironhack-datalabs/madrid-oct-2018')
+
+forks = repo.get_forks()
+list_forks = list(forks)
+print(list_forks)
+
+for fork in list_forks:
+    print(fork.get_languages())
